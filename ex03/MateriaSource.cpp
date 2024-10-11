509d0eef604fdf4fabbd71bed7d9824dba05d427
Andrey Arama <anarama@c2r1p6.42vienna.com>
2024-10-12 00:01:32 +0200

    working on MateriaSource class

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -6,7 +6,32 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 22:36:04 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/11 22:36:05 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/11 23:59:47 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource( void ) {
+	
+}
+
+MateriaSource::MateriaSource( const MateriaSource& other ) {
+	
+}
+
+MateriaSource::MateriaSource& operator=( const MateriaSource& other ) {
+
+}
+
+MateriaSource::~MateriaSource( void ) {
+	
+}
+
+void MateriaSource::learnMateria(AMateria *m) {
+
+}
+
+AMateria* MateriaSource::createMateria( std::string const& type) {
+	
+}
--- ex03/MateriaSource.hpp
+++ ex03/MateriaSource.hpp
@@ -6,15 +6,27 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 22:36:01 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/11 23:00:01 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/11 23:57:56 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATERIASOURCE_HPP
 # define MATERIASOURCE_HPP
 
+# include "AMateria.hpp"
+
 class MateriaSource: public IMateriaSource {
-	
+	private:
+		AMateria *_source_materias[4];
+
+	public:
+		MateriaSource( void );
+		MateriaSource( const MateriaSource& other );
+		MateriaSource& operator=( const MateriaSource& other );
+		~MateriaSource( void );
+
+		void learnMateria(AMateria *m);
+		AMateria* createMateria( std::string const& type);
 }
 
 #endif // MATERIASOURCE_HPP
